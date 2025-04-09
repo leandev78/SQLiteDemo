07bdc7d77ca9b2dce70b55c8bf8baf17c4d737f9
“leandev78” <leandev78@gmail.com>
2025-04-09 00:06:34 -0300

    Ejercicio donde se accede a una base de datos SQLITE y se hace uso de clases Abstracta, Interfaz, Polimorfismo. Corregido.

--- sqlite_2025/src/sqlite/code/model/Persona.java
+++ sqlite_2025/src/sqlite/code/model/Persona.java
@@ -24,7 +24,7 @@ public abstract class Persona implements Identificable {
  // Si la interfaz "Identificable" es implementada en la clase "Persona", ¿porque no estamos implementando su metodo "obtenerDocumento()" aquí?
  // Cuando una clase abstracta implementa una interfaz, tiene dos opciones:
  // 1. Implementar los métodos de la interfaz directamente en la clase abstracta.
- // 2. Dejar que las subclases concreten esos métodos, pero en ese caso, la clase abstracta también debe ser abstracta (como en tu caso, Persona).
+ // 2. Dejar que las subclases hijas concreten esos métodos.
  
  
 }
